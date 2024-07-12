d038cef8f5a51d0b7ad402a40743266aa950aa61
mutatum <www.ronan.c@gmail.com>
2024-07-12 22:16:46 +0200

    feat(window): initialize GLFW and OpenGL for window display
    - Initialized GLFW for window management.
    - Set up OpenGL context to render graphics.
    - Modified main.zig to create and display a basic GLFW window.

---
+++ src/main.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const glfw = @import("zglfw");
+const zopengl = @import("zopengl");
+
+const program_name = "basic_wireframe";
+
+pub fn main() !void {
+    glfw.init() catch |glfw_init_error| {
+        std.debug.print("GLFW initialization error: {}\n", .{glfw_init_error});
+        return;
+    };
+    defer glfw.terminate();
+
+    const gl_major = 4;
+    const gl_minor = 0;
+    glfw.windowHintTyped(.context_version_major, gl_major);
+    glfw.windowHintTyped(.context_version_minor, gl_minor);
+    glfw.windowHintTyped(.opengl_profile, .opengl_core_profile);
+    glfw.windowHintTyped(.opengl_forward_compat, true);
+    glfw.windowHintTyped(.client_api, .opengl_api);
+    glfw.windowHintTyped(.doublebuffer, true);
+
+    const window = try glfw.Window.create(600, 600, program_name, null);
+    defer window.destroy();
+
+    glfw.makeContextCurrent(window);
+
+    try zopengl.loadCoreProfile(glfw.getProcAddress, gl_major, gl_minor);
+
+    const gl = zopengl.bindings;
+
+    glfw.swapInterval(1);
+
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+
+        gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0.2, 0.6, 0.4, 1.0 });
+
+        window.swapBuffers();
+    }
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
